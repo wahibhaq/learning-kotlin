ac5aa1a8c99b7f04343d9ce1c1c97ea5295b2f00
Wahib-Ul-Haq <wahib.tech@gmail.com>
2017-09-28 08:52:56 +0200

    layout created and recyclerview integrated

--- app/src/main/java/com/wahib/learnkotlin/learningkotlin/MainActivity.kt
+++ app/src/main/java/com/wahib/learnkotlin/learningkotlin/MainActivity.kt
@@ -2,10 +2,19 @@ package com.wahib.learnkotlin.learningkotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.wahib.learnkotlin.learningkotlin.Adapter.ForecastListAdapter
 import com.wahib.learnkotlin.learningkotlin.util.Helper
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val items = listOf<String>(
+            "Monday: Sunny",
+            "Tuesday: Sunny",
+            "Wednesday: Raining"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +22,12 @@ class MainActivity : AppCompatActivity() {
         message.text = "Yo Kotlin!"
         inputPassword.setText(Helper.turnToStars())
         Helper.niceToast(this, "sample toast")
+
+        val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
     }
+
+
 
 }
