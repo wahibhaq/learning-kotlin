8efbae08b4238e2079618db3e02601b788306191
Wahib-Ul-Haq <wahib.tech@gmail.com>
2017-12-02 05:40:57 +0100

    advent of code day 1 part 1 solved

--- app/src/main/java/com/wahib/learnkotlin/learningkotlin/MainActivity.kt
+++ app/src/main/java/com/wahib/learnkotlin/learningkotlin/MainActivity.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.widget.Toast
 import com.wahib.learnkotlin.learningkotlin.Adapter.ForecastListAdapter
+import com.wahib.learnkotlin.learningkotlin.adventofcodechallenge.day1.Day1Challenge
 import com.wahib.learnkotlin.learningkotlin.util.Helper
+import io.reactivex.Observable
+import io.reactivex.functions.Action
+import io.reactivex.functions.Consumer
+import io.reactivex.internal.observers.ForEachWhileObserver
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.find
 
@@ -20,13 +27,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "Yo Kotlin!"
-        inputPassword.setText(Helper.turnToStars())
-        Helper.niceToast(this, "sample toast")
+//        message.text = "Yo Kotlin!"
+//        inputPassword.setText(Helper.turnToStars())
+//        Helper.niceToast(this, "sample toast")
+//
+//        val forecastList: RecyclerView = find(R.id.forecast_list)
+//        forecastList.layoutManager = LinearLayoutManager(this)
+//        forecastList.adapter = ForecastListAdapter(items)
 
-        val forecastList: RecyclerView = find(R.id.forecast_list)
-        forecastList.layoutManager = LinearLayoutManager(this)
-        forecastList.adapter = ForecastListAdapter(items)
+        var output: Int = 0
+        Day1Challenge.part1(applicationContext)?.subscribe(
+                { t -> output += t[0] },
+                { Log.e("error", "error")},
+                { message.text = output.toString()})
+
+
+//        message.text = Day1Challenge.solvePart1(applicationContext).toString()
+
+
+
 
     }
 
---
+++ app/src/main/java/com/wahib/learnkotlin/learningkotlin/adventofcodechallenge/day1/day1.kt
@@ -0,0 +1,60 @@
+package com.wahib.learnkotlin.learningkotlin.adventofcodechallenge.day1
+
+import android.content.Context
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.Predicate
+import io.reactivex.schedulers.Schedulers
+
+class Day1Challenge {
+
+    companion object {
+
+        //yes it fucking works
+        fun part1(context: Context?): Observable<MutableList<Int>>? {
+
+            val inputString = context?.assets?.open("input.txt")?.bufferedReader().use {
+                it?.readText()?.split("")
+            }
+
+            val numList: MutableList<Int> = ArrayList<Int>()
+            inputString
+                    ?.filter { s: String -> !s.equals("") }
+                    ?.map { s -> s.trim().replace(",", "") }
+                    ?.forEach { item: String -> numList.add(item.toInt())}
+
+            numList.add(numList.first())
+
+            return Observable.fromIterable(numList)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .buffer(2, 1)
+                    .filter(Predicate { buf -> buf.size == 2 && buf[0] == buf[1] })
+
+        }
+
+        //copied from reddit and worked fine
+        fun solvePart1(context: Context?): Int {
+
+            val inputString = context?.assets?.open("input.txt")?.bufferedReader().use {
+                it?.readText()?.split("")
+            }
+
+            val numList: MutableList<Int> = ArrayList<Int>()
+            inputString
+                    ?.filter { s: String -> !s.equals("") }
+                    ?.map { s -> s.trim().replace(",", "") }
+                    ?.forEach { item: String -> numList.add(item.toInt())}
+
+            return (numList + numList[0])   // Circular list, re-add [0] to the end so they pair up.
+                    .zipWithNext()
+                    .filter { it.first == it.second }
+                    .map { it.first}
+                    .sum()
+
+        }
+
+
+    }
+
+}
